68674ae88d3d158e19d0e2093eefdce2c14a022a
gulnaz <gulnazti@gmail.com>
2020-10-09 19:47:50 +0300

    apply 2_2_HW1_optional.patch

---
+++ src/main/java/ru/javawebinar/topjava/repository/MealRepository.java
@@ -0,0 +1,18 @@
+package ru.javawebinar.topjava.repository;
+
+import java.util.Collection;
+
+import ru.javawebinar.topjava.model.Meal;
+
+public interface MealRepository {
+    // null if not found, when updated
+    Meal save(Meal meal);
+
+    // false if not found
+    boolean delete(int id);
+
+    // null if not found
+    Meal get(int id);
+
+    Collection<Meal> getAll();
+}
