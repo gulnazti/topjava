13533b93217bd35f8f90725d59d200e355d69889
gulnaz <gulnazti@gmail.com>
2020-10-10 10:12:23 +0300

    apply 2_3_app_layers.patch

---
+++ src/main/java/ru/javawebinar/topjava/repository/UserRepository.java
@@ -0,0 +1,21 @@
+package ru.javawebinar.topjava.repository;
+
+import java.util.List;
+
+import ru.javawebinar.topjava.model.User;
+
+public interface UserRepository {
+    // null if not found, when updated
+    User save(User user);
+
+    // false if not found
+    boolean delete(int id);
+
+    // null if not found
+    User get(int id);
+
+    // null if not found
+    User getByEmail(String email);
+
+    List<User> getAll();
+}
---
+++ src/main/java/ru/javawebinar/topjava/web/user/AdminRestController.java
@@ -0,0 +1,38 @@
+package ru.javawebinar.topjava.web.user;
+
+import java.util.List;
+
+import ru.javawebinar.topjava.model.User;
+
+public class AdminRestController extends AbstractUserController {
+
+    @Override
+    public List<User> getAll() {
+        return super.getAll();
+    }
+
+    @Override
+    public User get(int id) {
+        return super.get(id);
+    }
+
+    @Override
+    public User create(User user) {
+        return super.create(user);
+    }
+
+    @Override
+    public void delete(int id) {
+        super.delete(id);
+    }
+
+    @Override
+    public void update(User user, int id) {
+        super.update(user, id);
+    }
+
+    @Override
+    public User getByMail(String email) {
+        return super.getByMail(email);
+    }
+}
