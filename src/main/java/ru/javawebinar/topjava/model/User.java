69a6ca6dac07938c0316d1bfed0993edb67700fd
gulnaz <gulnazti@gmail.com>
2020-11-20 17:22:53 +0300

    apply 7_13_jackson_object_mapper.patch

--- src/main/java/ru/javawebinar/topjava/model/User.java
+++ src/main/java/ru/javawebinar/topjava/model/User.java
@@ -7,7 +7,6 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.hibernate.annotations.BatchSize;
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
@@ -64,7 +63,7 @@ public class User extends AbstractNamedEntity {
 
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "user")//, cascade = CascadeType.REMOVE, orphanRemoval = true)
     @OrderBy("dateTime DESC")
-    @JsonIgnore
+//    @JsonIgnore
     private List<Meal> meals;
 
     public User() {
