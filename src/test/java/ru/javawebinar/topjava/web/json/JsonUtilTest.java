719f76e90a0a3cf4ca5109db4f9fe5b09444a078
gulnaz <gulnazti@gmail.com>
2020-11-20 17:44:41 +0300

    apply 7_14_json_assert_tests.patch

---
+++ src/test/java/ru/javawebinar/topjava/web/json/JsonUtilTest.java
@@ -0,0 +1,29 @@
+package ru.javawebinar.topjava.web.json;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import ru.javawebinar.topjava.model.Meal;
+
+import static ru.javawebinar.topjava.MealTestData.MEAL_MATCHER;
+import static ru.javawebinar.topjava.MealTestData.adminMeal1;
+import static ru.javawebinar.topjava.MealTestData.meals;
+
+class JsonUtilTest {
+
+    @Test
+    void readWriteValue() throws Exception {
+        String json = JsonUtil.writeValue(adminMeal1);
+        System.out.println(json);
+        Meal meal = JsonUtil.readValue(json, Meal.class);
+        MEAL_MATCHER.assertMatch(meal, adminMeal1);
+    }
+
+    @Test
+    void readWriteValues() throws Exception {
+        String json = JsonUtil.writeValue(meals);
+        System.out.println(json);
+        List<Meal> meals = JsonUtil.readValues(json, Meal.class);
+        MEAL_MATCHER.assertMatch(meals, meals);
+    }
+}
