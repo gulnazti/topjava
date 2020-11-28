f4078b24b27549a84d353f6c6dc5d0a00b85d740
gulnaz <gulnazti@gmail.com>
2020-11-28 11:13:26 +0300

    apply 8_0_fix.patch

--- src/test/java/ru/javawebinar/topjava/web/json/JsonUtilTest.java
+++ src/test/java/ru/javawebinar/topjava/web/json/JsonUtilTest.java
@@ -3,6 +3,7 @@ package ru.javawebinar.topjava.web.json;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
+import ru.javawebinar.topjava.MealTestData;
 import ru.javawebinar.topjava.model.Meal;
 
 import static ru.javawebinar.topjava.MealTestData.MEAL_MATCHER;
@@ -24,6 +25,6 @@ class JsonUtilTest {
         String json = JsonUtil.writeValue(meals);
         System.out.println(json);
         List<Meal> meals = JsonUtil.readValues(json, Meal.class);
-        MEAL_MATCHER.assertMatch(meals, meals);
+        MEAL_MATCHER.assertMatch(meals, MealTestData.meals);
     }
 }
